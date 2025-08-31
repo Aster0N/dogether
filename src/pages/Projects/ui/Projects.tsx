9c75d305489554c79d7edfe1219126bf87df1fb7
Aster0N <nikvas131@gmail.com>
2025-08-31 19:53:56 +0300

    styled sidebar collapsing and its button

--- src/pages/Projects/ui/Projects.tsx
+++ src/pages/Projects/ui/Projects.tsx
@@ -4,16 +4,16 @@ import cl from "./Projects.module.scss"
 const Projects = () => {
   return (
     <>
-      <h4>My projects</h4>
+      <h4 className={cl.header}>My projects</h4>
       <div className={cl.projects_wrapper}>
-        <CollapsableSidebar>
+        <CollapsableSidebar className={cl.project_sidebar}>
           <ul>
             <li>some</li>
             <li>thing</li>
             <li>here</li>
           </ul>
         </CollapsableSidebar>
-        <div>projects list</div>
+        <div className={cl.project_grid}>projects here...</div>
       </div>
     </>
   )
--- src/shared/ui/CollapsableSidebar/CollapsableSidebar.tsx
+++ src/shared/ui/CollapsableSidebar/CollapsableSidebar.tsx
@@ -4,9 +4,13 @@ import cl from "./CollapsableSidebar.module.scss"
 
 interface CollapsableSidebar {
   children: ReactNode
+  className?: string
 }
 
-const CollapsableSidebar: FC<CollapsableSidebar> = ({ children }) => {
+const CollapsableSidebar: FC<CollapsableSidebar> = ({
+  children,
+  className,
+}) => {
   const [isCollapsed, setIsCollapsed] = useState(false)
   const collapseSidebar = () => {
     setIsCollapsed(prev => !prev)
@@ -14,13 +18,13 @@ const CollapsableSidebar: FC<CollapsableSidebar> = ({ children }) => {
 
   return (
     <div
-      className={`${isCollapsed ? cl._collapsed : ""} ${cl.sidebar_wrapper}`}
+      className={`${isCollapsed ? cl._collapsed : ""} ${cl.sidebar_wrapper} ${
+        className ?? ""
+      }`}
     >
-      <div className={cl.collapse_btn_wrapper}>
-        <Button inline onClick={collapseSidebar}>
-          x
-        </Button>
-      </div>
+      <Button inline onClick={collapseSidebar} className={cl.collapse_btn}>
+        x
+      </Button>
       <div className={cl.sidebar_body}>{children}</div>
     </div>
   )
