c827b73da65f66bf99fe4755b922c7e636efa14b
Aster0N <nikvas131@gmail.com>
2026-01-19 18:46:03 +0300

    Fixed issue when text was copied while clicking on span, not whole element. Add toast message after text has copied

--- src/widgets/ProjectsList/ui/ProjectsList.tsx
+++ src/widgets/ProjectsList/ui/ProjectsList.tsx
@@ -1,58 +1,77 @@
 import type { Project } from "@/entities/project"
 import { useProjectStore } from "@/entities/project"
+import { Button, Toast, useToastOpenTrigger } from "@/shared"
 import { Copy } from "lucide-react"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import cl from "./ProjectsList.module.scss"
 
 const ProjectsList = () => {
   const { projectList, selectProject } = useProjectStore()
   const projectIdRefs = useRef<(HTMLElement | null)[]>([])
+  const [isIdCopying, setIsIdCopying] = useState(false)
+  const [isIdCopiedSuccessfully, setIsCopiedSuccessfully] = useState(false)
+  const { showToast, onToastClose } = useToastOpenTrigger(isIdCopying)
 
-  // TODO hook
+  const extractUUID = (str: string): string => {
+    const uuidRegex =
+      /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i
+    const match = str.match(uuidRegex)
+    return match ? match[0] : ""
+  }
+
+  // TODO decompose (hooks, Project component)
   const copyId = async (index: number) => {
-    console.log(index, projectIdRefs.current)
+    setIsIdCopying(true)
     if (!projectIdRefs.current || !projectIdRefs.current[index]) {
       return
     }
-    const projectId = projectIdRefs.current[index].innerText
+    const projectId = extractUUID(projectIdRefs.current[index].innerText)
     try {
       await navigator.clipboard.writeText(projectId)
-      // TODO toast
+      setIsCopiedSuccessfully(true)
     } catch (err) {
       console.error("error when copying project id", err)
+      setIsCopiedSuccessfully(false)
     }
+    setIsIdCopying(false)
   }
 
   return (
-    <div className={cl.project_grid}>
-      {Object.entries(projectList).map(
-        ([id, project]: [Project["id"], Project], i) => (
-          <div
-            className={cl.project_card}
-            key={id}
-            onClick={() => selectProject(id)}
-          >
-            <h6 className={cl.title}>{project.title}</h6>
-            <div className={`_small ${cl.project_id_container}`}>
-              <span
+    <>
+      <div className={cl.project_grid}>
+        {Object.entries(projectList).map(
+          ([id, project]: [Project["id"], Project], i) => (
+            <div
+              className={cl.project_card}
+              key={id}
+              onClick={() => selectProject(id)}
+            >
+              <h6 className={cl.title}>{project.title}</h6>
+              <Button
+                className={cl.project_id_container}
                 ref={el => {
                   if (projectIdRefs.current) {
                     projectIdRefs.current[i] = el
                   }
                 }}
-                className={cl.project_id}
                 onClick={() => copyId(i)}
               >
-                {id}
-              </span>
-              &nbsp;
-              <Copy size={18} />
+                <span className={cl.project_id}>{id}</span>
+                <Copy size={18} />
+              </Button>
+              <p className={cl.description}>{project.description}</p>
             </div>
-            <p className={cl.description}>{project.description}</p>
-          </div>
-        ),
+          ),
+        )}
+      </div>
+      {showToast && (
+        <Toast
+          onClose={onToastClose}
+          isSuccessCode={isIdCopiedSuccessfully}
+          messages={{ valid: "id copied", invalid: "id copying error" }}
+        />
       )}
-    </div>
+    </>
   )
 }
 
